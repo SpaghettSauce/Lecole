2f3a6c9a6bb2128609ed8dd4720d4f7fa4552047
SpaghettiSauce <dontstarve221@gmail.com>
2024-02-19 18:50:42 +0200

    Create OGE_TASK.py

---
+++ OGE_TASK.py
@@ -0,0 +1,12 @@
+'''
+Создаем перпеменную, которая будет высчитывать среднюю скорость middle_speed=0
+Создаем флаг bool flag = true
+Вносим первую цифру, по ней выполняем in range 4:
+    далее вводим переменную (скорость автомобиля)
+    если скорость < 60: flag = false
+    счетчик средней скорости middle_speed + скорость
+middle_speed /= 4
+if flag = true : print("YES")
+else: print ("No")
+print(middle_speed)
+'''
